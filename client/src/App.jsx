ff3a5d865c62a4bae9bdab0db369087dbb350080
asija123 <lakshayasija36@gmail.com>
2025-09-09 17:02:55 +0530

    bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
 import User from "./routes/User/User.jsx";
 import Admin from "./routes/Admin/Admin.jsx";
-import HomePage from "./pages/HomePage.jsx";
-
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/*" element={<User />} />
-          <Route path="/admin/*" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
+      <User />
+      <Admin />
     </Provider>
   );
 }
--- client/src/routes/User/User.jsx
+++ client/src/routes/User/User.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import RegisterPage from '../../pages/User/RegisterPage';
-import LoginPage from '../../pages/User/LoginPage';
-import ForgotPasswordPage from '../../pages/User/ForgotPage';
-import VerifyOtpPage from '../../pages/User/VerifyOptPage';
-import UserDashboardPage from '../../pages/User/UserDashboard';
-import UserProfilePage from '../../pages/User/UserProfilePage';
+import RegisterPage from "../../pages/User/RegisterPage";
+import LoginPage from "../../pages/User/LoginPage";
+import ForgotPasswordPage from "../../pages/User/ForgotPage";
+import VerifyOtpPage from "../../pages/User/VerifyOptPage";
+import UserDashboardPage from "../../pages/User/UserDashboard";
+import UserProfilePage from "../../pages/User/UserProfilePage";
+import HomePage from "../../pages/HomePage";
 
 const User = () => {
   return (
     <>
       <Routes>
-        <Route path="register" element={<RegisterPage/>}/>
-        <Route path="login" element={<LoginPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
 
-        <Route path="forgot" element={<ForgotPasswordPage/>}/>
-        <Route path="verify" element={<VerifyOtpPage/>}/>
+        <Route path="forgot" element={<ForgotPasswordPage />} />
+        <Route path="verify" element={<VerifyOtpPage />} />
 
-        <Route path="dashboard" element={<UserDashboardPage/>}/>
-        <Route path="profile" element={<UserProfilePage/>}/>
+        <Route path="dashboard" element={<UserDashboardPage />} />
+        <Route path="profile" element={<UserProfilePage />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default User
+export default User;
